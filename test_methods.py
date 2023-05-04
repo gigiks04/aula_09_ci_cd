02408ea8de397eee1755c72e4a3e9ef3f2fb9981
GIOVANNA FREIRE SOUZA <22.01453-5@maua.br>
2023-05-04 08:23:28 -0300

    exercicio calculadora Desenvolvimento Agil

--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,42 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_soma():
+    a = 2
+    b = 5
+
+    output = methods.soma(a, b)
+    
+    assert output == 7
+
+def test_subtracao():
+    a = 5
+    b = 2
+
+    output = methods.subtracao(a, b)
+    
+    assert output == 3
+
+def test_divisao():
+    a = 10
+    b = 2
+
+    output = methods.divisao(a, b)
+    
+    assert output == 5
+
+def test_multiplicacao():
+    a = 2
+    b = 5
+
+    output = methods.multiplicacao(a, b)
+    
+    assert output == 10
+
+def test_potencia():
+    a = 2
+
+    output = methods.potencia(a)
+    
+    assert output == 4
